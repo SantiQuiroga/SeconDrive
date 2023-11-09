3a763826f7a7e8f3b615cabe232ded35dcfc4295
DeidamiaFuentes <deidamia.fuentes.8698857@usalesiana.edu.bo>
2023-11-08 22:34:37 -0400

    feat: product card

---
+++ projects/client/src/components/product-Card/extra-components/AddToCartButton.tsx
@@ -0,0 +1,9 @@
+function AddToCartButton() {
+  return (
+    <button className="flex items-center justify-center bg-white rounded w-full p-2 text-base font-medium shadow-md">
+      Add to Cart +
+    </button>
+  );
+}
+
+export default AddToCartButton;
