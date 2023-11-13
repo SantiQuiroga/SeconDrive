20c9f558a2d72abd2d364ff5e429422f0eba80cd
Diego Michel Roca <aa6132788@gmail.com>
2023-11-13 19:43:53 -0400

    refactor: delete react component unit tests

--- projects/client/test/App.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-
-import App from '@/App';
-
-test('renders the App component', () => {
-  const { getByText } = render(<App />);
-  const headingElement = getByText('SeconDrive');
-  expect(headingElement).toBeInTheDocument();
-});
