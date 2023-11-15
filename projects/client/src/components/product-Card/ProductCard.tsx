73c6261303de39436940772b323928f1882ba458
DeidamiaFuentes <deidamia.fuentes.8698857@usalesiana.edu.bo>
2023-11-15 17:28:08 -0400

    fix: refactore price

--- projects/client/src/components/product-Card/ProductCard.tsx
+++ projects/client/src/components/product-Card/ProductCard.tsx
@@ -8,10 +8,15 @@ function ProductCard({
   discount,
   alt,
 }: ProductCardProps) {
+  const discountedPrice = Number(price) - Number(price) * (discount / 100);
   return (
-    <div className='relative w-[230px] text-black bg-[#DED9E1] p-5 rounded-lg'>
-      <div className='relative bg-white rounded flex items-center justify-center'>
-        <img src={image} alt={alt} className='h-24 rounded object-cover' />
+    <div className='relative w-[230px] text-black bg-[rgb(222,217,225)] p-5 rounded-lg'>
+      <div className='relative bg-white rounded flex items-center justify-center h-24 overflow-hidden'>
+        <img
+          src={image}
+          alt={alt}
+          className='max-h-24 w-max-full object-cover rounded'
+        />
       </div>
       {discount > 0 && <OfferBadge discount={discount} />}
 
@@ -19,9 +24,23 @@ function ProductCard({
         <p className='font-bold text-3xl flex items-center justify-center font-ropa'>
           {children}
         </p>
-        <div>
-          <span className='flex w-full text-base font-ropa'>Price</span>
-          <span className='flex w-full text-base font-ropa'>$ {price}</span>
+        <div className='text-lg'>
+          <span className='flex w-full font-ropa'>Price</span>
+          <div className='flex justify-start'>
+            <span>[USD]</span>
+            <span
+              className={`flex font-ropa ml-1 ${
+                discount > 0 ? 'line-through' : ''
+              }`}
+            >
+              {price}
+            </span>
+            {discount > 0 && (
+              <span className='font-ropa text-[#DC0700] ml-1'>
+                {discountedPrice.toFixed(2)}
+              </span>
+            )}
+          </div>
         </div>
       </div>
 
