0fc40222103e4a9a59d13a75443ac7eb2c06d2bb
DeidamiaFuentes <deidamia.fuentes.8698857@usalesiana.edu.bo>
2023-11-16 00:11:09 -0400

    test: fix tests

--- projects/client/test/components/product-Card/ProductCard.spec.tsx
+++ projects/client/test/components/product-Card/ProductCard.spec.tsx
@@ -16,7 +16,7 @@ test('renders ProductCard correctly', () => {
 
   expect(getByText('Product Name')).toBeInTheDocument();
   expect(getByText('Price')).toBeInTheDocument();
-  expect(getByText('$ 29.99')).toBeInTheDocument();
+  expect(getByText('29.99')).toBeInTheDocument();
 });
 
 test('renders OfferBadge when discount is provided', () => {
@@ -31,7 +31,7 @@ test('renders OfferBadge when discount is provided', () => {
     </ProductCard>
   );
 
-  expect(getByText('15% off')).toBeInTheDocument();
+  expect(getByText('15%')).toBeInTheDocument();
 });
 
 test('does not render OfferBadge when discount is 0', () => {
@@ -46,5 +46,41 @@ test('does not render OfferBadge when discount is 0', () => {
     </ProductCard>
   );
 
-  expect(queryByText('15% off')).toBeNull();
+  expect(queryByText('15%')).toBeNull();
+});
+
+test('renders the discount price when discount is provided', () => {
+  const { getByText } = render(
+    <ProductCard
+      image='https://w7.pngwing.com/pngs/457/630/png-transparent-location-logo-location-computer-icons-symbol-location-miscellaneous-angle-heart.png'
+      price='29.99'
+      alt='Product Alt'
+      discount={10}
+    >
+      Product Name
+    </ProductCard>
+  );
+
+  const originalPriceText = getByText('29.99');
+  const discountedPriceText = getByText('26.99');
+
+  expect(originalPriceText).toBeInTheDocument();
+  expect(discountedPriceText).toBeInTheDocument();
+  expect(originalPriceText).toHaveClass('line-through');
+});
+
+test('does not render the discount price when discount is provided', () => {
+  const { queryByText } = render(
+    <ProductCard
+      image='https://w7.pngwing.com/pngs/457/630/png-transparent-location-logo-location-computer-icons-symbol-location-miscellaneous-angle-heart.png'
+      price='29.99'
+      alt='Product Alt'
+      discount={0}
+    >
+      Product Name
+    </ProductCard>
+  );
+
+  const discountedPrice = (29.99 - 29.99 * 0.1).toFixed(2);
+  expect(queryByText(discountedPrice)).toBeNull();
 });
