8dbbe994ad2491a5e6f16b824a7a7c0303b75e52
yoloxd101 <santiago.quiroga.8794944@usalesiana.edu.bo>
2023-11-16 11:28:40 -0400

    feat: category page

--- projects/client/src/App.tsx
+++ projects/client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Layout from './app/layouts/Layout';
+import Category from './app/pages/category/Category';
 import Home from './app/pages/home/Home';
 import Search from './app/pages/search/Search';
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/search/:search' element={<Search />} />
+        <Route path='/:category/:name' element={<Category />} />
       </Routes>
     </Layout>
   );
