131944e4a674fd5c9734e1b4297eb95555171d25
Santiago Quiroga <quiroga.default.santiago@gmail.com>
2023-11-23 18:26:45 -0400

    fix: console erros problems

--- projects/client/src/app/pages/category/Category.tsx
+++ projects/client/src/app/pages/category/Category.tsx
@@ -237,7 +237,6 @@ function Category() {
             onPageChange={(e: { selected: number }) =>
               handlePageChange(e.selected)
             }
-            forcePage={currentPage}
             containerClassName='w-full flex justify-center gap-10 items-center text-3xl'
             activeClassName='text-[#0038FF]'
             previousLabel=''
