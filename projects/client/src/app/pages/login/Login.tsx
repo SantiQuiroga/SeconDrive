d742251424aa28835a83614cbc9a32e18adeffdf
Santiago Quiroga <quiroga.default.santiago@gmail.com>
2023-11-28 17:30:01 -0400

    feat: add mock validation

--- projects/client/src/app/pages/login/Login.tsx
+++ projects/client/src/app/pages/login/Login.tsx
@@ -1,18 +1,37 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function Login() {
-  const [error] = useState<boolean>(true);
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    try {
+      if (password === 'password' && email === 'email@test.com') {
+        setError(false);
+      } else {
+        setError(true);
+      }
+    } catch (err) {
+      setError(true);
+    }
+  };
 
   return (
     <main className='bg-black h-screen w-screen flex justify-center items-center font-ropa'>
-      <section className='bg-[#DED9E1] h-fit w-1/2 lg:w-1/4 p-10 flex flex-col gap-5'>
+      <section className='bg-[#794299] h-fit w-1/2 lg:w-1/4 p-10 flex flex-col gap-5'>
         <h1 className='text-[26px] w-full flex justify-center'>Login</h1>
-        <form className='flex flex-col gap-5'>
+        <form onSubmit={handleLogin} className='flex flex-col gap-5'>
           <label htmlFor='email' className='flex flex-col'>
             <span className='text-lg'>Email</span>
             <input
               id='email'
               type='email'
+              value={email}
+              onChange={e => setEmail(e.target.value)}
               className='outline-none px-2 py-3 rounded-md text-lg'
               placeholder='Enter your email...'
               required
@@ -22,18 +41,24 @@ function Login() {
             <span className='text-lg'>Password</span>
             <input
               id='password'
-              type='password'
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              onChange={e => setPassword(e.target.value)}
               placeholder='Enter your password...'
               className='outline-none px-2 py-3 rounded-md text-lg'
               required
             />
+            <button
+              type='button'
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? 'Hide' : 'Show'} Password
+            </button>
           </label>
           <div className='flex justify-center flex-col items-center gap-4'>
             <button
               type='submit'
-              className='mt-3 bg-white py-3 px-16 rounded-md text-[20px] text-lg w-fit transition duration-100 active:transform
-              active:scale-90 cursor-pointer hover:bg-slate-white/80
-              active:bg-white'
+              className='mt-3 bg-white py-3 px-16 rounded-md text-[20px] text-lg w-fit transition duration-100 active:transform active:scale-90 cursor-pointer hover:bg-slate-white/80 active:bg-white'
             >
               login
             </button>
