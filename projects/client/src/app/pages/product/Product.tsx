f6b09a9fefddc724fb24c31277a2ea11b8a79701
CristianJQ <cristian.joel.quispia.quipe@gmail.com>
2023-11-29 00:32:35 -0400

    fix: product page information and database connection

--- projects/client/src/app/pages/product/Product.tsx
+++ projects/client/src/app/pages/product/Product.tsx
@@ -1,59 +1,59 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Category, getAllCategories } from 'src/app/api/CategoryAPI';
+import CheckboxPrice from 'src/app/components/price-checkbox/PriceCheckbox';
 
-import jsonData from '../home/assets/data.json';
-
-interface Product {
-  id: string;
-  categoryId: string;
-  name: string;
-  description: string;
-  brand: string;
-  price: string;
-  image: string;
-  stock: string;
-  discount: string;
-  unitsold: string;
-}
-
-const items: string[] = [
-  'Engines',
-  'Electrical System',
-  'Wheels and Tires',
-  'Filters',
-  'Radiator',
-  'Air Bags',
-  'Brake Components',
-  'Belts and Hoses',
-  'Electrical Components',
-  'Suspension',
-];
+import { getAllProducts, ProductApi } from '@/api/productApi';
 
 function ProductPage() {
   const { id } = useParams<{ id: string }>();
-  const [product, setProduct] = useState<Product | undefined>(undefined);
-  const [isUsd, setIsUsd] = useState(true);
-  const [isBol, setIsBol] = useState(false);
-  const [priceUsd, setPriceUsd] = useState(0);
-  const [priceBol, setPriceBol] = useState(0);
+  const [product, setProduct] = useState<ProductApi | undefined>(undefined);
+  const [categoryList, setCategories] = useState<Category[] | undefined>(
+    undefined
+  );
+  const [isAvailable, setIsAvailable] = useState(true);
+
+  const [changePrice, setChangePrice] = useState(false);
+
+  const [price, setPrice] = useState(0);
 
   useEffect(() => {
-    if (!jsonData.some(item => item.id === id)) return;
-    setProduct(jsonData.find(item => item.id === id) as Product);
-    setPriceUsd(Number(jsonData.find(item => item.id === id)?.price));
-    setPriceBol(Number(jsonData.find(item => item.id === id)?.price) * 6.97);
+    getAllProducts()
+      .then((res: Response) => res.json())
+      .then((data: ProductApi[]) => {
+        setProduct(data.find(item => item.id === Number(id)) as ProductApi);
+        if (Number(data.find(item => item.id === Number(id))?.stock === 0))
+          setIsAvailable(false);
+        setPrice(Number(data.find(item => item.id === Number(id))?.price));
+      })
+      .catch((err: Error) => {
+        return err;
+      });
   }, [id]);
 
+  useEffect(() => {
+    getAllCategories()
+      .then((res: Response) => res.json())
+      .then((data: Category[]) => {
+        setCategories(data);
+      })
+      .catch((err: Error) => {
+        return err;
+      });
+  }, []);
+
   const toggleIsUsd = () => {
-    if (isUsd) return;
-    setIsUsd(true);
-    setIsBol(false);
+    setChangePrice(false);
+    if (changePrice) {
+      setPrice(price / 6.97);
+    }
   };
 
   const toggleIsBol = () => {
-    if (isBol) return;
-    setIsBol(true);
-    setIsUsd(false);
+    setChangePrice(true);
+    if (!changePrice) {
+      setPrice(price * 6.97);
+    }
   };
 
   if (!product)
@@ -64,60 +64,57 @@ function ProductPage() {
     );
 
   return (
-    <div className='h-full w-full flex'>
-      <div className='h-full w-1/2 grid place-content-center'>
-        <img src={product.image} alt={product.name} height={336} width={472} />
+    <div className='w-full flex'>
+      <div className='w-1/2 grid place-content-center'>
+        <img src={product.image} alt={product.name} height={300} width={472} />
       </div>
-      <div className='h-full w-1/2 flex flex-col gap-10 px-20 py-10 bg-[#ded9e1]'>
-        <h2 className='w-full text-center text-[60px]'>{product.name}</h2>
-        <div className='flex gap-32 px-20 items-center'>
-          <div>
-            <label htmlFor='isUSD' className='flex items-center text-[25px]'>
-              USD&nbsp;
-              <input
-                id='isUSD'
-                className='rounded-full appearance-none bg-white w-6 h-6 border-2 border-gray-300 checked:bg-[#ff0000] checked:border-white focus:outline-none'
-                type='checkbox'
-                checked={isUsd}
-                onChange={toggleIsUsd}
-              />
-            </label>
-          </div>
-
-          <div>
-            <label htmlFor='isBol' className='flex items-center text-[25px]'>
-              Bs&nbsp;
-              <input
-                id='isBol'
-                className='rounded-full appearance-none bg-white w-6 h-6 border-2 border-gray-300 checked:bg-[#ff0000] checked:border-white focus:outline-none'
-                type='checkbox'
-                checked={isBol}
-                onChange={toggleIsBol}
-              />
-            </label>
-          </div>
-
+      {Number(product.discount) > 0 && (
+        <div className='right-0 top-24 absolute text-2xl px-5 py-7  font-bold bg-[#DC0700] text-white first-letter: font-ropa text-[55px]'>
+          {product.discount}%
+        </div>
+      )}
+      <div className='w-1/2 flex flex-col gap-8 px-20 py-10 bg-[#ded9e1] text-[40px]'>
+        <h2 className='text-center text-[60px]'>{product.name}</h2>
+        <div className='flex gap-32'>
+          <CheckboxPrice
+            valuePrice='USD'
+            checked={!changePrice}
+            onToggle={toggleIsUsd}
+          />
+          <CheckboxPrice
+            valuePrice='BOL'
+            checked={changePrice}
+            onToggle={toggleIsBol}
+          />
           <span className='text-[25px]'>Change 1 (USD) = 6,97 (Bs)</span>
         </div>
-        <h3 className='text-[40px]'>Description:</h3>
+        <h3>Description:</h3>
         <p className='text-[30px]'>{product.description}</p>
-        <h3 className='text-[40px]'>
+        <h3>
           Brand:&nbsp;&nbsp;&nbsp;
           <span className='text-[30px]'>{product.brand}</span>
         </h3>
-        <h3 className='text-[40px]'>
+        <h3>
           Category:&nbsp;&nbsp;&nbsp;
           <span className='text-[30px]'>
-            {items[Number(product.categoryId) - 1]}
+            {categoryList?.find(item => item.id === Number(product.id))?.name}
           </span>
         </h3>
-        <h3 className='text-[40px]'>
+        <h3>
           Price:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <span className='text-[35px]'>
-            {isUsd
-              ? `[USD] ${priceUsd.toFixed(0)}`
-              : `[BOL] ${priceBol.toFixed(0)}`}
-          </span>
+          {!changePrice ? `[USD]` : `[BOL]`}&nbsp;
+          {product.discount > 0 ? (
+            <>
+              <span className='line-through text-[35px]'>
+                {price.toFixed(2)}
+              </span>
+              <span className='text-red-600'>
+                &nbsp;{(price - (price * product.discount) / 100).toFixed(2)}
+              </span>
+            </>
+          ) : (
+            price.toFixed(2)
+          )}
         </h3>
         <button
           type='button'
@@ -125,6 +122,9 @@ function ProductPage() {
         >
           Add to Cart +
         </button>
+        <h3 className='text-center text-[30px]'>
+          {isAvailable ? 'Available' : 'Out of stock'}
+        </h3>
       </div>
     </div>
   );
