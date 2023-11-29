e2d2e5e72e971e5bdab8acd93ebd18afbe3e9bfa
DeidamiaFuentes <deidamia.fuentes.8698857@usalesiana.edu.bo>
2023-11-28 22:04:46 -0400

    feat: card view

--- projects/client/src/globals/types.d.ts
+++ projects/client/src/globals/types.d.ts
@@ -1,17 +1,36 @@
 export {};
 
 declare global {
-  type ProductCardProps = {
+  interface ProductCardProps {
     children: React.ReactNode;
     id: string;
     image: string;
     price: number;
     discount: number;
     alt: string;
-  };
+  }
+
   type CheckboxPriceProps = {
     valuePrice: string;
     checked: boolean;
     onToggle: () => void;
   };
+
+  interface CartProductCardProps {
+    children: React.ReactNode;
+    id: string;
+    image: string;
+    price: number;
+    discount: number;
+    alt: string;
+    stock: number;
+    initialValue?: number;
+  }
+
+  interface NumericInputProps {
+    max: number;
+    onChange: (value: number) => void;
+    onError: (error: string) => void;
+    initialValue?: number;
+  }
 }
