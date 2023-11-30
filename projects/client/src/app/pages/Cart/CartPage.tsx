3d7952b970948b9205708a1081a3a1e744f9d6bf
Ronaldo-Miguel-Angel-Mendoza-Mallcu <ronaldo.mendoza.60098@usalesiana.edu.bo>
2023-11-30 12:48:51 -0400

    feat: shopping cart display

--- projects/client/src/app/pages/Cart/CartPage.tsx
+++ projects/client/src/app/pages/Cart/CartPage.tsx
@@ -1,8 +1,12 @@
 function CartPage() {
   return (
-    <div className='flex flex-col justify-center items-center w-full h-full'>
-      <h1>Cart Page</h1>
-      <p>Coming soon...</p>
+    <div className='h-full w-full flex flex-col px-40 '>
+      <div className='flex flex-col items-center text-6xl mt-12'>
+        <h1>My Cart</h1>
+        <div className='w-full text-5xl flex items-center mt-6 '>
+          <p>Items :</p>
+        </div>
+      </div>
     </div>
   );
 }
