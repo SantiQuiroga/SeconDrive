80c1f53d17cd203f065e03e0d93d02d9302cc82c
DMichelR <aa6132788@gmail.com>
2023-11-30 14:24:10 -0400

    fix: not resoult found message

--- projects/client/src/app/pages/search/Search.tsx
+++ projects/client/src/app/pages/search/Search.tsx
@@ -10,12 +10,14 @@ function Search() {
   const [subData, setSubData] = useState<Product[]>([]);
   const [currentPage, setCurrentPage] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<Error>();
   const { search } = useParams<{ search: string }>();
   const itemsPerPage = 25;
 
   useEffect(() => {
     if (!search) return;
+    setIsLoading(true);
     GetProductsByName(search)
       .then(res => {
         return res.json();
@@ -23,8 +25,12 @@ function Search() {
       .then((data: { products: Product[] }) => {
         setProducts(data.products);
         setTotalPages(Math.ceil(data.products.length / itemsPerPage));
+        setIsLoading(false);
       })
-      .catch((err: Error) => setError(err));
+      .catch((err: Error) => {
+        setError(err);
+        setIsLoading(false);
+      });
   }, [search]);
 
   useEffect(() => {
@@ -49,45 +55,59 @@ function Search() {
   return (
     <div className='h-full w-full flex flex-col gap-2 px-40 py-10'>
       <div className='h-full flex flex-col justify-center gap-10'>
-        <div
-          className={`grid gap-20 h-full w-full ${
-            products.length === 0 ? '' : 'grid-cols-5'
-          }`}
-        >
-          {products.length === 0 ? (
-            <div className='h-full grid place-content-center w-full'>
-              <h1 className='text-2xl font-bold text-center'>
-                No results Found for {search}
-              </h1>
+        {isLoading && (
+          <div className='h-full grid place-content-center w-full'>
+            <h1 className='text-2xl font-bold text-center overflow-hidden'>
+              Loading...
+            </h1>
+          </div>
+        )}
+        {!isLoading && !error && (
+          <>
+            <div
+              className={`grid gap-20 h-full w-full ${
+                products.length === 0 ? '' : 'grid-cols-5'
+              }`}
+            >
+              {products.length === 0 ? (
+                <div className='h-full grid place-content-center w-full'>
+                  <h1 className='text-2xl font-bold text-center overflow-hidden'>
+                    No results Found for{' '}
+                    {search && search.length > 20
+                      ? `${search.slice(0, 20)}...`
+                      : search}
+                  </h1>
+                </div>
+              ) : (
+                subData.map(card => (
+                  <ProductCard
+                    key={card.id}
+                    id={card.id}
+                    discount={Number(card.discount)}
+                    image={card.image}
+                    price={Number(card.price)}
+                    alt={card.name}
+                    stock={card.stock}
+                  >
+                    {card.name}
+                  </ProductCard>
+                ))
+              )}
             </div>
-          ) : (
-            subData.map(card => (
-              <ProductCard
-                key={card.id}
-                id={card.id}
-                discount={Number(card.discount)}
-                image={card.image}
-                price={Number(card.price)}
-                alt={card.name}
-                stock={card.stock}
-              >
-                {card.name}
-              </ProductCard>
-            ))
-          )}
-        </div>
-        <div className={`${products.length < 26 ? 'hidden' : ''}`}>
-          <ReactPaginate
-            pageCount={totalPages}
-            onPageChange={(e: { selected: number }) =>
-              handlePageChange(e.selected)
-            }
-            containerClassName='w-full flex justify-center gap-10 items-center text-3xl'
-            activeClassName='text-[#0038FF]'
-            previousLabel=''
-            nextLabel=''
-          />
-        </div>
+            <div className={`${products.length < 26 ? 'hidden' : ''}`}>
+              <ReactPaginate
+                pageCount={totalPages}
+                onPageChange={(e: { selected: number }) =>
+                  handlePageChange(e.selected)
+                }
+                containerClassName='w-full flex justify-center gap-10 items-center text-3xl'
+                activeClassName='text-[#0038FF]'
+                previousLabel=''
+                nextLabel=''
+              />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
