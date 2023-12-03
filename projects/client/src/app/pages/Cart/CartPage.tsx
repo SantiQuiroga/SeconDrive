600e635af1c5d69b5de9db1f6056ce4cf420b776
Santiago Quiroga <quiroga.default.santiago@gmail.com>
2023-12-02 21:29:25 -0400

    fix: show  cart summary

--- projects/client/src/app/pages/Cart/CartPage.tsx
+++ projects/client/src/app/pages/Cart/CartPage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import {
   CartProduct,
@@ -23,73 +24,46 @@ import CheckboxPrice from '../../components/checkbox-price/checkboxprice';
 import CardImage from './assets/card.png';
 
 function CartPage() {
+  const navigate = useNavigate();
+  const { items } = cartStore.getState();
+  const [products, setProducts] = useState([] as Product[]);
   const [price, setPrice] = useState(0);
-  const [totalItems, setItems] = useState(0);
-  const [cartProductList, setCartProductList] = useState<CartProduct[]>([]);
+  const [totalItems, setTotalItems] = useState(0);
   const [changePrice, setChangePrice] = useState(false);
   const [estimatedTax, setEstimatedTax] = useState(7);
   const costShipping = 10;
 
-  const toggleIsUsd = () => {
-    setChangePrice(false);
-    if (changePrice) {
-      setPrice(price / 6.97);
-      setEstimatedTax(estimatedTax / 6.97);
-    }
-  };
-
-  const toggleIsBol = () => {
-    setChangePrice(true);
-    if (!changePrice) {
-      setPrice(price * 6.97);
-      setEstimatedTax(estimatedTax * 6.97);
-    }
-  };
-
   useEffect(() => {
+    setProducts(items);
+
     getAllCartProductsByCartId('1')
-      .then((res: Response) => res.json())
+      .then(res => res.json())
       .then((data: CartProduct[]) => {
-        setCartProductList(data);
+        const itemsq = data.reduce(
+          (total, cartProduct) => total + cartProduct.quantity,
+          0
+        );
+        setTotalItems(itemsq);
       })
-      .catch((err: Error) => {
-        return err;
+      .catch(err => {
+        console.error(err);
       });
 
     getCartTotalPriceByCartId('1')
-      .then((res: Response) => res.json())
+      .then(res => res.json())
       .then((data: number) => {
         setPrice(data);
       })
-      .catch((err: Error) => {
-        return err;
+      .catch(err => {
+        console.error(err);
       });
-    const items = cartProductList.reduce(
-      (total, cartProduct) => total + cartProduct.quantity,
-      0
-    );
-    setItems(items);
-  }, [cartProductList]);
-  //-----
-  const { items } = cartStore.getState();
-  const [products, setProducts] = useState<Product[]>([] as Product[]);
-
-  useEffect(() => {
-    setProducts(items);
-  }, [items]);
+  }, [items, products]);
 
   const handleRemove = (id: number) => {
     cartStore.getState().removeItem(id);
     setProducts(state => state.filter(item => item.id !== id));
   };
 
-  const [price, setPrice] = useState(0);
-  const [totalItems, setItems] = useState(0);
-  const [cartProductList, setCartProductList] = useState<CartProduct[]>([]);
-  const [changePrice, setChangePrice] = useState(false);
-  const [estimatedTax, setEstimatedTax] = useState(7);
-  const costShipping = 10;
-
   const toggleIsUsd = () => {
     setChangePrice(false);
     if (changePrice) {
@@ -105,31 +79,6 @@ function CartPage() {
       setEstimatedTax(estimatedTax * 6.97);
     }
   };
-
-  useEffect(() => {
-    getAllCartProductsByCartId('1')
-      .then((res: Response) => res.json())
-      .then((data: CartProduct[]) => {
-        setCartProductList(data);
-      })
-      .catch((err: Error) => {
-        return err;
-      });
-
-    getCartTotalPriceByCartId('1')
-      .then((res: Response) => res.json())
-      .then((data: number) => {
-        setPrice(data);
-      })
-      .catch((err: Error) => {
-        return err;
-      });
-    const items = cartProductList.reduce(
-      (total, cartProduct) => total + cartProduct.quantity,
-      0
-    );
-    setItems(items);
-  }, [cartProductList]);
 
   return (
     <div className='w-full h-full py-10 px-32 flex flex-col gap-10'>
@@ -161,58 +110,61 @@ function CartPage() {
             })}
           </div>
         </div>
-      </div>
-      <div className='flex flex-col justify-center items-center w-full h-full'>
-        <h1>Cart Page</h1>
-        <p>Coming soon...</p>
-        <div className='gap-10 flex-col flex text-black border border-black p-10 rounded-lg'>
-          <h3 className='text-[35px] text-center'>Shipping Details</h3>
-          <div className='flex gap-32 px-40 items-center'>
-            <CheckboxPrice
-              valuePrice='USD'
-              checked={!changePrice}
-              onToggle={toggleIsUsd}
-            />
-            <CheckboxPrice
-              valuePrice='BOL'
-              checked={changePrice}
-              onToggle={toggleIsBol}
-            />
+        <div className='w-2/5 pt-96'>
+          <div className=' flex flex-col justify-center items-center h-1/4'>
+            <div className='gap-10 flex-col flex text-black border border-black p-10 rounded-lg'>
+              <h3 className='text-[35px] text-center'>Order Summary</h3>
+              <div className='flex gap-32 px-40 items-center'>
+                <CheckboxPrice
+                  valuePrice='USD'
+                  checked={!changePrice}
+                  onToggle={toggleIsUsd}
+                />
+                <CheckboxPrice
+                  valuePrice='BOL'
+                  checked={changePrice}
+                  onToggle={toggleIsBol}
+                />
+              </div>
+              <h3 className='text-[35px]'>Order Summary</h3>
+              <div className='flex justify-between text-[25px]'>
+                Items:
+                <span> {totalItems} </span>
+              </div>
+              <div className='flex justify-between text-[25px]'>
+                Shipping and Handling:
+                <span>
+                  {changePrice
+                    ? `${(Number(costShipping) * 6, 97).toFixed(2)} [BOL]`
+                    : `${Number(costShipping).toFixed(2)} [USD]`}
+                </span>
+              </div>
+              <hr className='border-black border' />
+              <div className='flex justify-between text-[25px]'>
+                Total Before Tax:
+                <span>{price}</span>
+              </div>
+              <div className='flex justify-between text-[25px]'>
+                Estimated Tax:
+                <span>{estimatedTax}</span>
+              </div>
+              <hr className='border-black border' />
+              <div className='flex justify-between text-[35px]'>
+                Total:
+                <span>{price + estimatedTax}</span>
+              </div>
+              <button
+                type='button'
+                className='flex justify-center bg-yellow-400 py-2 text-[40px] font-black'
+                onClick={() => {
+                  navigate('/payment');
+                }}
+              >
+                PAY&nbsp;&nbsp;&nbsp;
+                <img src={CardImage} alt='card' width={55} />
+              </button>
+            </div>
           </div>
-          <h3 className='text-[35px]'>Order Summary</h3>
-          <div className='flex justify-between text-[25px]'>
-            Items:
-            <span> {totalItems} </span>
-          </div>
-          <div className='flex justify-between text-[25px]'>
-            Shipping and Handling:
-            <span>
-              {changePrice
-                ? `${(Number(costShipping) * 6, 97).toFixed(2)} [BOL]`
-                : `${Number(costShipping).toFixed(2)} [USD]`}
-            </span>
-          </div>
-          <hr className='border-black border' />
-          <div className='flex justify-between text-[25px]'>
-            Total Before Tax:
-            <span>{price}</span>
-          </div>
-          <div className='flex justify-between text-[25px]'>
-            Estimated Tax:
-            <span>{estimatedTax}</span>
-          </div>
-          <hr className='border-black border' />
-          <div className='flex justify-between text-[35px]'>
-            Total:
-            <span>{price + estimatedTax}</span>
-          </div>
-          <button
-            type='button'
-            className='flex justify-center bg-yellow-400 py-2 text-[40px] font-black'
-          >
-            PAY&nbsp;&nbsp;&nbsp;
-            <img src={CardImage} alt='card' width={55} />
-          </button>
         </div>
       </div>
     </div>
