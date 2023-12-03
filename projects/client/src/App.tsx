31b0d8981b718cc42a2aa816589e3b9f35d76e0f
Ronaldo-Miguel-Angel-Mendoza-Mallcu <87450546+Ronaldo-Miguel-Angel-Mendoza-Mallcu@users.noreply.github.com>
2023-12-03 02:12:45 -0400

    Revert "fix: push fixes"

--- projects/client/src/App.tsx
+++ projects/client/src/App.tsx
@@ -4,7 +4,6 @@ import Layout from './app/layouts/Layout';
 import CartPage from './app/pages/Cart/CartPage';
 import Category from './app/pages/category/Category';
 import Home from './app/pages/home/Home';
-import InvoicePage from './app/pages/invoice/InvoicePage';
 import Login from './app/pages/login/Login';
 import PaymentPage from './app/pages/payment/PaymentPage';
 import ProductPage from './app/pages/product/Product';
@@ -20,7 +19,6 @@ function App() {
         <Route path='/product/:id' element={<ProductPage />} />
         <Route path='/cart' element={<CartPage />} />
         <Route path='/payment' element={<PaymentPage />} />
-        <Route path='/invoice' element={<InvoicePage />} />
       </Route>
       <Route path='/login' element={<Login />} />
     </Routes>
